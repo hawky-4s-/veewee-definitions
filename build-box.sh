620b16640e0e896b70ffc175925cb338cb534579
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-04-04 13:43:54 +0200

    Tweak build-box.sh

--- build-box.sh
+++ build-box.sh
@@ -7,9 +7,17 @@ set -e
 
 BOX="${1:?box definition missing}"
 
+# Show commands
+set -x
+
 cd ..
+
+# Build box
 bundle install
 bundle exec veewee vbox build --force --nogui "$BOX"
 bundle exec veewee vbox export --force "$BOX"
+
+# Add box to Vagrant
 vagrant box remove "$BOX" virtualbox || true
 vagrant box add "$BOX" "$BOX.box"
+vagrant box list
