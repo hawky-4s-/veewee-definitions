efaacd645facbf92b569063bff7d0963670c36db
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-04-05 09:58:10 +0200

    Minor tweaks in add-box.sh

--- add-box.sh
+++ add-box.sh
@@ -3,14 +3,10 @@
 # Usage: add-box.sh <box definition>
 # Writtten by Mathias Lafeldt <mlafeldt@jimdo.com>
 
-set -e
+set -e -x
 
 BOX="${1:?box definition missing}"
 
-# Show commands
-set -x
-
-# Add box to Vagrant
 vagrant box remove "$BOX" virtualbox || true
 vagrant box add "$BOX" "../$BOX.box"
 vagrant box list
